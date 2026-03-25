1584fb26178eafb9e00a59a3862ac57182eea9f3
Rafael Marco Manubay <rafaelmarcomanubay@gmail.com>
2026-03-25 21:20:05 +1300

    util: Add Jest + React Testing Library setup
    Add initial testing support: introduce jest.config.js (using next/jest) and jest.setup.ts (loads @testing-library/jest-dom). Update README with a new "Testing" section documenting the test stack, recommended structure, guidelines, and how to run tests (npm test, npm run test:watch). Update package.json to expose test scripts and include testing dev dependencies.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+}
+
+module.exports = createJestConfig(customJestConfig)
